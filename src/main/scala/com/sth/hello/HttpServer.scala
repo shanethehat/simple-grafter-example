fbcd60e89882480b0b848d648877d38238001436
Shane Auckland <shane.auckland@gmail.com>
2017-01-09 09:39:34 +0000

    Start the web server

--- src/main/scala/com/sth/hello/HttpServer.scala
+++ src/main/scala/com/sth/hello/HttpServer.scala
@@ -1,16 +1,24 @@
 package com.sth.hello
 
+import cats.Eval
 import cats.data.Reader
 import org.http4s.server.blaze.BlazeBuilder
 import org.zalando.grafter.GenericReader._
+import org.zalando.grafter.{Start, StartResult, Stop, StopResult}
 
-case class HttpServer(config: HttpConfig, route: Routes) {
+case class HttpServer(config: HttpConfig, route: Routes) extends Start with Stop {
   private lazy val builder: BlazeBuilder =
     BlazeBuilder
       .bindHttp(config.port, config.host)
       .mountService(route.httpService)
 
   private lazy val server = builder.run
+
+  def start: Eval[StartResult] = {
+    StartResult.eval("http4s")(server)
+  }
+
+  def stop: Eval[StopResult] = StopResult.eval("http4s")(server.shutdownNow())
 }
 
 object HttpServer {
--- src/main/scala/com/sth/hello/Main.scala
+++ src/main/scala/com/sth/hello/Main.scala
@@ -1,5 +1,7 @@
 package com.sth.hello
 
+import java.util.concurrent.CountDownLatch
+
 import org.zalando.grafter._
 // these imports are used, intellij just can't tell
 import GenericReader._
@@ -8,8 +10,17 @@ import cats.data.Reader
 case class Application(httpServer: HttpServer)
 
 object Main extends App {
+  val latch = new CountDownLatch(1)
 
   val application: Application = GenericReader[ApplicationConfig, Application]
     .run(ApplicationConfig(HttpConfig("localhost", 8080)))
 
+  Rewriter.start(application).value
+
+  sys.addShutdownHook {
+    Rewriter.stop(application).value
+    latch.countDown()
+  }
+
+  latch.await()
 }
